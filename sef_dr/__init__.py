814bd9563af24af473c658ca798c4d1e14f08daf
Nikolaos Passalis <passalis@users.noreply.github.com>
2017-07-09 11:04:53 +0300

    Import all similarity targets

--- sef_dr/__init__.py
+++ sef_dr/__init__.py
@@ -1,6 +1,6 @@
 # License: MIT License https://github.com/passalis/sef/blob/master/LICENSE.txt
 
-from targets import sim_target_copy, sim_target_supervised, sim_target_svm
+from targets import *
 
 from linear import LinearSEF
 from kernel import KernelSEF
